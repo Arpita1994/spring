a3be7dd3d185b25ab71f0a9f970c081da5106c5c
Arpitha kulkarni <arpita.kulkarni9@gmail.com>
2018-05-19 20:31:08 +0530

    spring core practise first checkin

---
+++ src/main/java/com/arpitha/beans/MainApp.java
@@ -0,0 +1,12 @@
+package com.arpitha.beans;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+   public static void main(String[] args) {
+      ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+      obj.getMessage();
+   }
+}
